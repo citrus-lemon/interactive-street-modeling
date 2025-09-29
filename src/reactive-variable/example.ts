36e67bc1b7bf977bcfd971d5514ed71508f5a3a2
WindProphet <lihanyuan1996@gmail.com>
2025-09-30 00:13:37 +0900

    Enhance input component to support selection options and improve rendering logic

--- src/reactive-variable/example.ts
+++ src/reactive-variable/example.ts
@@ -1,13 +1,19 @@
 import { html, render } from "lit";
-import { block, variable } from ".";
+import { block, InputVariable, Variable, variable } from ".";
 
 const a = variable(3);
 const b = a.apply((k) => `lit: ${k}`);
 const d = variable("qeqw");
 const c = block({ d, b, e: 3 }).lit(({ d, b, e }) => html`d@${d} b${b} e ${e}`);
+const q = variable(false);
+const sel: InputVariable<"bfs" | "dfs"> = variable("bfs");
 
 render(
-  html` ${a.input({ label: "as", min: "3", max: "15", step: "2" })}
-    <div>${c}</div>`,
+  html` <div>${a.input({ label: "as", min: "3", max: "15", step: "2" })}</div>
+    <div>${d.input({ label: "asdf" })}</div>
+    <div>${q.input({ label: "qq" })}</div>
+    <div>${sel.input({ selection: ["bfs", "dfs"] })}</div>
+
+    <div>${c} - - ${sel.view}</div>`,
   document.querySelector("#root") as HTMLElement
 );
--- src/reactive-variable/input.ts
+++ src/reactive-variable/input.ts
@@ -4,6 +4,7 @@ import { spread } from "@open-wc/lit-helpers";
 
 type InputComponentOptionType = Partial<HTMLInputElement> & {
   label?: string;
+  selection?: string[] | { [key: string]: string };
 };
 
 declare module "./core" {
@@ -20,32 +21,77 @@ export function inputComponent<T>(
   val: InputVariable<T>,
   options?: InputComponentOptionType
 ): TemplateResult<any> {
-  const { label, ...args } = options || {};
+  const { label, selection, ...args } = options || {};
   let callback;
   let type: HTMLInputElement["type"];
   if (typeof val.value === "number") {
-    type = "range";
+    type = args.type || "range";
     callback = (e: Event) => {
       const target = e.target as HTMLInputElement;
       val.value = Number(target.value) as T;
     };
+    return html`<label>
+      ${label && `${label}: `} ${val.view}
+      <input
+        ${spread({
+          type,
+          ...args,
+          ".value": String(val.value),
+          "@input": callback,
+        })}
+      />
+    </label>`;
+  } else if (selection) {
+    return html`
+      <label style="display: flex; align-items: center; gap: 5px;">
+        ${label}
+        <select
+          .value=${val.value as string}
+          @change=${(e: Event) => {
+            val.value = (e.target as HTMLSelectElement).value as T;
+          }}
+        >
+          ${(Array.isArray(selection)
+            ? selection.map((a) => [a, a])
+            : Object.entries(selection)
+          ).map(
+            ([value, showName]) =>
+              html`<option value=${value}>${showName}</option>`
+          )}
+        </select>
+      </label>
+    `;
   } else if (typeof val.value === "string") {
     type = "text";
     callback = (e: Event) => {
       const target = e.target as HTMLInputElement;
       val.value = target.value as T;
     };
+    return html`<label>
+      ${label && `${label}: `}
+      <input
+        ${spread({
+          type,
+          ...args,
+          ".value": String(val.value),
+          "@input": callback,
+        })}
+      />
+    </label>`;
+  } else if (typeof val.value === "boolean") {
+    type = "checkbox";
+    return html`<label>
+      <input
+        type="checkbox"
+        .checked=${val.value}
+        @change=${(e: Event) => {
+          val.value = (e.target as HTMLInputElement).checked as T;
+        }}
+        ${spread(args)}
+      />
+      ${label}
+    </label>`;
   } else {
     return html`Variable ${val.view}`;
   }
-  const attrs = spread({
-    type,
-    ...args,
-    ".value": String(val.value),
-    "@input": callback,
-  });
-  return html`
-    ${label && html`<label>${label}</label>: ${val.view}`}
-    <input ${attrs} />
-  `;
 }
