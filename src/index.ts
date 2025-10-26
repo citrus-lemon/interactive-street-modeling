4cc708a9f83ea2d2a8f3c704d944b9a5b709c1c6
Lemon <lihanyuan1996@gmail.com>
2025-10-26 16:11:46 +0900

    Fix anchor tag href in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ document.querySelector("#root")!.innerHTML = `
   <h1>Pages</h1>
   <ul>
     ${__MPA_ENTRIES__
-      .map((name) => `<li><a href="/${name}">${name}</a></li>`)
+      .map((name) => `<li><a href="${name}">${name}</a></li>`)
       .join("")}
   </ul>
 `;
